ef5c2e548a36447fa60ed32a66489045b92c353b
Tin <tinmarkoviccs@gmail.com>
2017-01-30 11:39:51 +0100

    Modified URLs to allow only numbers

--- texas_dashboard/urls.py
+++ texas_dashboard/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'texas_dashboard.views',
 
-    url(r'^notifications/(?P<notification_name>[^/]+)/$', 'manage_notifications', name='remove_notification'),
-    url(r'^lomodules/(?P<module_name>[^/]+)/$', 'manage_modules', name='manage_modules'),
+    url(r'^notifications/(?P<notification_id>[0-9]+)/$', 'manage_notifications', name='remove_notification'),
+    url(r'^lomodules/(?P<module_id>[0-9]+)/$', 'manage_modules', name='manage_modules'),
 )
